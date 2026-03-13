5872dfb95101a4ac5a5d037fa69f3acf1e19ba4f
junesnest-cpu <junesnest@gmail.com>
2026-03-13 10:40:12 +0900

    Update daily.js

--- pages/daily.js
+++ pages/daily.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { ref, onValue } from "firebase/database";
 import { db } from "../lib/firebaseConfig";
-import useIsMobile from "../lib/useIsMobile";
+import useIsMobile from "../lib/useismobile";
 
 const WARD_STRUCTURE = {
   2: { name: "2병동", rooms: [
