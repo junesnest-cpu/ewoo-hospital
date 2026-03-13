36d9e540f42b76d3a114b686b07c48a4fec52d27
junesnest-cpu <junesnest@gmail.com>
2026-03-13 10:07:34 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,10 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>병동 현황 관리</title>
+        <title>이우요양병원 병동관리</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="shortcut icon" href="/favicon.png" />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;600;700;800&display=swap"
           rel="stylesheet"
